01a2e188aed7e3341e49f1bf7d503885b543b052
RamQ216 <hugomramirez06@gmail.com>
2026-06-01 23:29:08 -0300

    feat:creacion del flujo de datos entre peticion y modelos

---
+++ controllers/topicController.js
@@ -0,0 +1,73 @@
+const topicModel=require("../models/topicModel");
+
+function showHome(req,res){
+    //pedimos todos los modelos
+    const topics=topicModel.getAlltopics();
+    //renderizamos y pasmos los temas
+    res.render("index",{topics});
+}
+
+function createTopic(req,res){
+    //del json sacamos title y descripcion
+    const{title,description}=req.body;
+
+    //creamos el nuevo tema
+    topicModel.createTopic(title,description);
+    
+    //redireccion a la pagina principal
+    res.redirect("/");
+
+};
+
+
+function showEditForm(req, res){
+    //sacamos del parametro el id
+    const {id} = req.params;
+    //buscamos el tema por id
+    const topic = topicModel.getTopicById(id);
+    //verificamos que exista
+    if(!topic){
+        return res.redirect("/");
+    }
+    //renderizamos y pasamos el tema
+    res.render("edit",{ topic });
+};
+
+function updateTopic(req,res){
+    const {id}= req.params;
+
+    const{title,description}=req.body;
+
+    topicModel.updateTopic(id,title,description);
+
+    res.redirect("/");
+};
+
+function deleteTopic(req,res){
+    const {id}= req.params;
+
+    topicModel.deleteTopic(id);
+
+    res.redirect("/");
+
+};
+
+function voteTopic(req,res){
+    const {id}= req.params;
+
+    topicModel.voteTopic(id);
+
+    res.redirect("/");
+};
+
+
+module.exports = {
+  showHome,
+  createTopic,
+  showEditForm,
+  updateTopic,
+  deleteTopic,
+  voteTopic
+};
+
+
